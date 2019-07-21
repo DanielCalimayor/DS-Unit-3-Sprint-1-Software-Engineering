e7ca2bde72794324fd70ebb2dab3cdf3af7c42f8
Daniel Calimayor-Montero <46464886+DanielCalimayor@users.noreply.github.com>
2019-07-21 12:02:36 -0400

    Sprint 1

---
+++ module1-python-modules-packages-and-environments/Unit 3 Sprint 1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from random import randint
+
+
+class Product:
+   
+    def __init__(self, name, price=10, weight=20,
+                 flammability=.5, identifier=randint(1000000, 9999999)):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+
+    def sab(self):
+        steal_ratio = self.price / self.weight
+        if steal_ratio < .5:
+            return 'not so stealable...'
+        elif steal_ratio < 1:
+            return 'a little stealable.'
+        else:
+            return 'very stealable!'
+
+    def explode(self):
+        volatility = self.flammability * self.weight
+        if volatility < 10:
+            return 'small explosion'
+        elif volatility < 50:
+            return 'big explosive'
+        else:
+            return 'HUGE explosive'
+
+
+# In[ ]:
+
+
+
+
